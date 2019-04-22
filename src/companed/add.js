5a2f0c6ea75fb8ed3739a5bcede45a260fc01805
your name <you@example.com>
2019-04-22 23:01:19 +0400

    my-app

--- src/companed/add.js
+++ src/companed/add.js
@@ -8,15 +8,25 @@ class Add extends Component {
         surname : "bilgi yox",
         age : "bilgi yox"
     }
+
+    constructor(props){
+      super(props);
+
+      this.state = {
+        test: 2
+      }
+    }
     render() {
         const {name,surname,age} = this.props
       return (
-        <div>
+        <div className="section">
+            <h4> addlar <i className="far fa-angry"></i></h4>
             <ul>
-                <li>add : {name}</li>
+                <li>add : {name} </li>
                 <li>soy add : {surname} </li>
                 <li> yas : {age}</li>
             </ul>
+            <p>{this.state.test}</p>
         </div>
       );
     }
